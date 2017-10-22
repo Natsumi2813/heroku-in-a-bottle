965551545f5a9510865cc1e80244414e31b5159e
Kristján O. Ragnarsson <korri1313@gmail.com>
2017-10-22 13:32:36 +0000

    modifyied error

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def post_api(id):
 def static(path):
     return bottle.static_file(path, root='./st')
 
-bottle.run(host='0.0.0.0', port=argv[1])
+bottle.run(host='0.0.0.0', port=argv[1], reloader=False)
