a9b6686cceaa7c7a13259eef70546d1f04bd34b7
Kristján O. Ragnarsson <korri1313@gmail.com>
2017-10-22 13:06:04 +0000

    added sys argv

--- app.py
+++ app.py
@@ -4,8 +4,7 @@
 import bottle
 import json
 import urllib.request
-
-from bottle import request, route, response, get, post, template, error, HTTPError, static_file
+from sys import argv
 api = {'car': 'http://apis.is/car?number={}', 'company': 'http://apis.is/company?name={}'}
 links = {'car': 'Car', 'company': 'Company'}
 
@@ -16,43 +15,43 @@ def get_api_data(api_):
     return data
 
 
-@error(404)
+@bottle.error(404)
 def error404(error):
-    return template('error.html')
+    return bottle.template('error.html')
 
 
-@route('/')
+@bottle.route('/')
 def index():
-    return template('inde.html')
+    return bottle.template('inde.html')
 
 
-@route('/404')
+@bottle.route('/404')
 def rais_404():
-    raise HTTPError(404)
+    raise bottle.HTTPError(404)
 
 
-@get('/<id>')
+@bottle.get('/<id>')
 def get_api(id):
     try:
-        return template('{}.html'.format(id), {'multi': False})
+        return bottle.template('{}.html'.format(id), {'multi': False})
     except:
-        raise HTTPError(404)
+        raise bottle.HTTPError(404)
 
 
-@post('/<id>')
+@bottle.post('/<id>')
 def post_api(id):
-    if len(list(request.forms)) > 0:
-        item = [request.forms.get(b) for b in [x for x in request.forms]]
+    if len(list(bottle.request.forms)) > 0:
+        item = [bottle.request.forms.get(b) for b in [x for x in bottle.request.forms]]
         info = get_api_data(api[id].format(item[0]))
         info['results'][0].update({'multi': True, 'id': id})
-        return template('{}_p.html'.format(id), info['results'][0])
+        return bottle.template('{}_p.html'.format(id), info['results'][0])
     else:
         info = get_api_data(api[id])
         info['results']['multi'] = False
-        return template('', info)
+        return bottle.template('', info)
 
-@get('/s/<path:re:.*\.(png|jpg|json|css)>')
+@bottle.get('/s/<path:re:.*\.(png|jpg|json|css)>')
 def static(path):
-    return static_file(path, root='./st')
+    return bottle.static_file(path, root='./st')
 
 bottle.run(host='0.0.0.0', port=argv[1])
